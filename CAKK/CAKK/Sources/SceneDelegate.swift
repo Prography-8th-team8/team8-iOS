0f440e3b76af120b683431c797a1acb65272968f
Wallaby <avocado34.131@gmail.com>
2023-04-11 00:16:18 +0900

    [Feat] Mock SplashViewController 구현
    <Implementation>
    - SplashViewController 구현
    - 예시 Splash lottie aniamtion 추가
    - launch screen accent color로 채움
    
    <Assets>
    - asset에 accentColor 추가
    
    <Podfile>
    - lottie-ios pod 추가
    
    <Lint>
    - 마커 위에 2개의 스페이징을 주기 위해서 vertical_whitespace 규칙 제거
    
    Ref: #2
    close #2

--- CAKK/CAKK/Sources/SceneDelegate.swift
+++ CAKK/CAKK/Sources/SceneDelegate.swift
@@ -8,14 +8,42 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+  
+  // MARK: - Constants
+  
+  enum Constants {
+    static let splashFadeOutDuration = 0.2.f
+  }
+  
+  
+  // MARK: - Properties
+  
   var window: UIWindow?
-
+  
+  
+  // MARK: - LifeCycle
+  
   func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
     guard let windowScene = (scene as? UIWindowScene) else { return }
     let window = UIWindow(windowScene: windowScene)
-    window.rootViewController = MainViewController()
+    
+    // 루트뷰를 메인뷰로 설정
+    let mainViewController = MainViewController()
+    window.rootViewController = mainViewController
     self.window = window
     window.makeKeyAndVisible()
+    
+    // 스플래시 시작
+    let splashViewController = SplashViewController()
+    splashViewController.modalPresentationStyle = .overCurrentContext // splashViewController.view.alpha = 0 했을 시에 배경이 검정색이 아닌 투명으로 보여주기 위함
+    mainViewController.present(splashViewController, animated: false)
+    
+    splashViewController.startSplash {
+      UIView.animate(withDuration: Constants.splashFadeOutDuration) {
+        splashViewController.view.alpha = 0
+      } completion: { _ in
+        splashViewController.dismiss(animated: true)
+      }
+    }
   }
 }
