07f4ba370a25bb2e237b7ecec1e95db874568cf5
Mason Kim <qwerty3345@naver.com>
2023-05-07 18:29:29 +0900

    [Feat] NetworkService 구현
    Publisher를 반환하는 networkService 구현

--- CAKK/CAKK/Sources/Network/CakeEndpoint.swift
+++
@@ -1,8 +0,0 @@
-//
-//  CakeEndpoint.swift
-//  CAKK
-//
-//  Created by Mason Kim on 2023/05/07.
-//
-
-import Foundation
--- CAKK/CAKK/Sources/Network/NetworkService.swift
+++ CAKK/CAKK/Sources/Network/NetworkService.swift
@@ -5,4 +5,30 @@
 //  Created by Mason Kim on 2023/05/07.
 //
 
-import Foundation
+import Moya
+import Combine
+
+final class NetworkService {
+  
+  // MARK: - Properties
+  
+  private let provider = MoyaProvider<CakeAPI>()
+  private let decoder = JSONDecoder()
+  
+  // MARK: - Public
+  
+  private func request<T: Decodable>(_ target: CakeAPI, type: T.Type) -> AnyPublisher<T, MoyaError> {
+    provider.requestPublisher(target)
+      .filterSuccessfulStatusCodes()
+      .compactMap { [weak self] in
+        guard let self else { return nil }
+        return try? self.decoder.decode(T.self, from: $0.data)
+      }
+      .eraseToAnyPublisher()
+  }
+  
+}
+
+struct CakeMockData: Decodable {
+  let cakeNames: [String]
+}
