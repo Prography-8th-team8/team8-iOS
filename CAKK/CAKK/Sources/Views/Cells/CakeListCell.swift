071a618cac5e264a4f08ebc3b2a4068ef14e6a2f
Mason Kim <qwerty3345@naver.com>
2023-05-18 13:38:42 +0900

    [Chore] 케이크리스트 셀 configure타입 맞게 수정

--- CAKK/CAKK/Sources/Views/Cells/CakeListCell.swift
+++ CAKK/CAKK/Sources/Views/Cells/CakeListCell.swift
@@ -94,7 +94,7 @@ final class CakeShopCollectionCell: HighlightableCell {
   
   public func configure(_ item: CakeShop) {
     shopNameLabel.text = item.name
-    districtLocationLabel.text = item.district
+    districtLocationLabel.text = item.district.koreanName
     locationLabel.text = item.location
     
     configureCakeShopTypeStackView(item.cakeShopTypes)
