f8d57715dd2eeda7b2016acdbe85463160ca0f0d
Mason Kim <qwerty3345@naver.com>
2023-06-04 17:08:28 +0900

    [Feat] 얼럿 extension 구현

---
+++ CAKK/CAKK/Sources/Utils/Extensions/UIViewController+ShowAlert.swift
@@ -0,0 +1,42 @@
+//
+//  UIViewController+ShowAlert.swift
+//  CAKK
+//
+//  Created by Mason Kim on 2023/06/04.
+//
+
+import UIKit
+
+extension UIViewController {
+  func showFailAlert(with title: String) {
+    let alert = UIAlertController(title: title,
+                                  message: nil, preferredStyle: .alert)
+    let confirmAction = UIAlertAction(title: "확인", style: .default)
+    alert.addAction(confirmAction)
+    
+    present(alert, animated: true)
+  }
+  
+  /// 사용자에게 Alert로 확인/취소를 물어보고 결과값을 completion으로 반환
+  /// - Parameters:
+  ///   - title: 물어볼 내용 title
+  ///   - message: 본문 내용
+  ///   - completion: 확인 시 true, 취소 시 false를 completion으로 반환
+  func showAskAlert(title: String,
+                    message: String? = nil,
+                    completion: @escaping (Bool) -> Void) {
+    let alert = UIAlertController(title: title,
+                                  message: message, preferredStyle: .alert)
+    
+    let confirmAction = UIAlertAction(title: "확인", style: .default) { _ in
+      completion(true)
+    }
+    let cancelAction = UIAlertAction(title: "취소", style: .cancel) { _ in
+      completion(false)
+    }
+    alert.addAction(confirmAction)
+    alert.addAction(cancelAction)
+    
+    present(alert, animated: true)
+  }
+}
