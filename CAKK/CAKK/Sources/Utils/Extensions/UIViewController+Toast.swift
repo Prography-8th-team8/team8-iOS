41c85d4261e96dd179798f23a590f7abe51845f2
Mason Kim <qwerty3345@naver.com>
2023-06-22 18:16:23 +0900

    [Refactor] 토스트 띄우기 디자인에 맞게 수정

--- CAKK/CAKK/Sources/Utils/Extensions/UIViewController+Toast.swift
+++ CAKK/CAKK/Sources/Utils/Extensions/UIViewController+Toast.swift
@@ -10,32 +10,39 @@ import SnapKit
 import Then
 
 extension UIViewController {
+  
+  private enum ToastMetric {
+    static let horizontalPadding = 16.f
+    static let verticalPadding = 10.f
+    static let bottomInset = 30.f
+    static let height = 50.f
+  }
+  
   func showToast(with message: String, delay: TimeInterval = 1) {
-    let horizontalPadding = 20.f
-    let verticalPadding = 10.f
-    
     let toastView = UIView().then {
-      $0.backgroundColor = .black
+      $0.backgroundColor = R.color.gray_80()
       $0.layer.cornerRadius = 10
     }
+    
     let toastLabel = UILabel().then {
       $0.text = message
       $0.textColor = .white
       $0.textAlignment = .center
       $0.numberOfLines = 0
-      $0.font = .pretendard()
+      $0.font = .pretendard(size: 16)
     }
     
     toastView.addSubview(toastLabel)
     toastLabel.snp.makeConstraints {
-      $0.horizontalEdges.equalToSuperview().inset(horizontalPadding)
-      $0.verticalEdges.equalToSuperview().inset(verticalPadding)
+      $0.horizontalEdges.equalToSuperview().inset(ToastMetric.horizontalPadding)
+      $0.verticalEdges.equalToSuperview().inset(ToastMetric.verticalPadding)
     }
     
     view.addSubview(toastView)
     toastView.snp.makeConstraints {
-      $0.horizontalEdges.equalToSuperview().inset(horizontalPadding)
-      $0.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom).inset(20)
+      $0.horizontalEdges.equalToSuperview().inset(ToastMetric.horizontalPadding)
+      $0.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom).inset(ToastMetric.bottomInset)
+      $0.height.equalTo(ToastMetric.height)
     }
     
     toastView.alpha = 0.0
