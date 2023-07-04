92960ef515cb7debec6588b4fb9fb1f4fe153f4b
Mason Kim <qwerty3345@naver.com>
2023-07-04 21:42:07 +0900

    [Feat] 세그먼티드컨트롤 선택 시 언더라인 구현

--- CAKK/CAKK/Sources/Views/UnderlineSegmentedControl.swift
+++ CAKK/CAKK/Sources/Views/UnderlineSegmentedControl.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 
+import Combine
+
 import SnapKit
 import Then
 
@@ -14,49 +16,34 @@ import Then
 final class UnderlineSegmentedControl: UISegmentedControl {
   
   
+  // MARK: - Properties
+  
+  private var cancellables = Set<AnyCancellable>()
+  
+  
   // MARK: - UI Components
   
-  private lazy var underlineView = UIView().then { view in
-    let width = self.bounds.size.width / CGFloat(numberOfSegments)
-    let height = 2.0
-    let xPosition = CGFloat(selectedSegmentIndex * Int(width))
-    let yPosition = bounds.size.height - 1.0
-    let frame = CGRect(x: xPosition, y: yPosition, width: width, height: height)
-    let view = UIView(frame: frame)
-    view.backgroundColor = .green
-    addSubview(view)
+  private let underlineView = UIView().then { view in
+    view.backgroundColor = .black
   }
+  
+  private var leadingConstraints: Constraint?
   
   
   // MARK: - Initialization
   
   override init(frame: CGRect) {
     super.init(frame: frame)
-    self.removeBackgroundAndDivider()
+    setup()
   }
   
   override init(items: [Any]?) {
     super.init(items: items)
-    self.removeBackgroundAndDivider()
+    setup()
   }
   
   required init?(coder: NSCoder) {
     fatalError()
-  }
-  
-  
-  // MARK: - Lifecycle
-  
-  override func layoutSubviews() {
-    super.layoutSubviews()
-    
-    let underlineFinalXPosition = (self.bounds.width / CGFloat(self.numberOfSegments)) * CGFloat(self.selectedSegmentIndex)
-    UIView.animate(
-      withDuration: 0.1,
-      animations: {
-        self.underlineView.frame.origin.x = underlineFinalXPosition
-      }
-    )
   }
   
   
@@ -68,5 +55,46 @@ final class UnderlineSegmentedControl: UISegmentedControl {
     setBackgroundImage(image, for: .selected, barMetrics: .default)
     setBackgroundImage(image, for: .highlighted, barMetrics: .default)
     setDividerImage(image, forLeftSegmentState: .selected, rightSegmentState: .normal, barMetrics: .default)
+  }
+  
+  // MARK: - Setup
+  
+  private func setup() {
+    removeBackgroundAndDivider()
+    setupLayout()
+    setupTextStyle()
+    selectedSegmentIndex = 0
+    bindSelectSegementedIndex()
+  }
+  
+  private func setupLayout() {
+    addSubview(underlineView)
+    underlineView.snp.makeConstraints {
+      $0.top.equalTo(snp.bottom).inset(1)
+      $0.width.equalTo(snp.width).dividedBy(numberOfSegments)
+      $0.height.equalTo(2)
+      
+      leadingConstraints = $0.leading.equalToSuperview().constraint
+    }
+  }
+  
+  private func setupTextStyle() {
+    // 선택 시 bold 처리
+    setTitleTextAttributes([.font: UIFont.pretendard(size: 16)], for: .normal)
+    setTitleTextAttributes([.font: UIFont.pretendard(size: 16, weight: .bold)], for: .selected)
+  }
+  
+  private func bindSelectSegementedIndex() {
+    selectedSegmentIndexPublisher.sink { [weak self] index in
+      guard let self = self else { return }
+      let widthPerSegment = bounds.size.width / CGFloat(numberOfSegments)
+      let xPosition = widthPerSegment * CGFloat(index)
+      
+      leadingConstraints?.update(inset: xPosition)
+      UIView.animate(withDuration: 0.3, animations: {
+        self.layoutIfNeeded()
+      })
+    }
+    .store(in: &cancellables)
   }
 }
