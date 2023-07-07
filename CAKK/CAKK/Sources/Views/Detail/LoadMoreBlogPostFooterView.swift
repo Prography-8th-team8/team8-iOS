25f27da82c2bab47fb5479ec73c4c94061dedd8a
Mason Kim <qwerty3345@naver.com>
2023-07-07 14:06:06 +0900

    [Feat] 블로그포스트 더보기 푸터뷰 정의

---
+++ CAKK/CAKK/Sources/Views/Detail/LoadMoreBlogPostFooterView.swift
@@ -0,0 +1,32 @@
+//
+//  LoadMoreBlogPostFooterView.swift
+//  CAKK
+//
+//  Created by Mason Kim on 2023/07/07.
+//
+
+import UIKit
+
+import Then
+
+
+final class LoadMoreBlogPostFooterView: UICollectionReusableView {
+  let button = UIButton().then {
+    $0.titleLabel?.font = .pretendard(size: 14, weight: .bold)
+    $0.layer.borderColor = R.color.gray_5()?.cgColor
+    $0.layer.borderWidth = 2
+    $0.layer.cornerRadius = 8
+    $0.setTitle("블로그 리뷰 더 보기", for: .normal)
+    $0.setTitleColor(.black, for: .normal)
+  }
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    addSubview(button)
+    button.frame = bounds
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
