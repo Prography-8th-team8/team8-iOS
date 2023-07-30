ce378ba9897e4632a4947b0009ad2e17f5411abf
Mason Kim <qwerty3345@naver.com>
2023-07-30 18:20:28 +0900

    [Feat] 팝업 컨트롤러 구현

---
+++ CAKK/CAKK/Sources/ViewControllers/PopUpViewController.swift
@@ -0,0 +1,175 @@
+//
+//  PopUpViewController.swift
+//  CAKK
+//
+//  Created by Mason Kim on 2023/07/30.
+//
+
+import UIKit
+
+import Combine
+import CombineCocoa
+
+import SnapKit
+import Then
+
+
+class PopUpViewController: UIViewController {
+  
+  // MARK: - Properties
+  
+  private let titleText: String
+  private let descriptionText: String?
+  
+  private var confirmAction: () -> Void
+  private var cancelAction: (() -> Void)?
+  
+  private var cancellables = Set<AnyCancellable>()
+  
+  // MARK: - UI
+  
+  private let contentView = UIView().then {
+    $0.backgroundColor = .white
+    $0.layer.cornerRadius = 14
+  }
+  
+  private lazy var titleLabel = UILabel().then {
+    $0.text = titleText
+    $0.textAlignment = .center
+    $0.font = .pretendard(size: 18, weight: .semiBold)
+  }
+  
+  private lazy var descriptionLabel = descriptionText == nil ? nil : UILabel().then {
+    $0.text = descriptionText
+    $0.textAlignment = .center
+    $0.font = .pretendard()
+  }
+  
+  private lazy var titleDescriptionStackView = UIStackView().then { stack in
+    stack.spacing = 8
+    stack.axis = .vertical
+    
+    stack.addArrangedSubview(titleLabel)
+    if let descriptionLabel {
+      stack.addArrangedSubview(descriptionLabel)
+    }
+  }
+  
+  private let cancelButton = UIButton().then {
+    $0.setTitle("취소", for: .normal)
+    $0.setTitleColor(R.color.gray_60(), for: .normal)
+    $0.titleLabel?.font = .pretendard(size: 16)
+    $0.backgroundColor = R.color.gray_10()
+    $0.layer.cornerRadius = 12
+  }
+  
+  private let confirmButton = UIButton().then {
+    $0.setTitle("확인", for: .normal)
+    $0.setTitleColor(.white, for: .normal)
+    $0.titleLabel?.font = .pretendard(size: 16, weight: .semiBold)
+    $0.backgroundColor = R.color.pink_100()
+    $0.layer.cornerRadius = 12
+  }
+  
+  private lazy var buttonStackView = UIStackView(arrangedSubviews: [
+    cancelButton, confirmButton
+  ]).then {
+    $0.axis = .horizontal
+    $0.spacing = 8
+    $0.distribution = .fillEqually
+  }
+  
+  
+  // MARK: - Initializers
+  
+  init(titleText: String,
+       descriptionText: String? = nil,
+       confirmAction: @escaping () -> Void,
+       cancelAction: (() -> Void)? = nil) {
+    self.titleText = titleText
+    self.descriptionText = descriptionText
+    self.confirmAction = confirmAction
+    self.cancelAction = cancelAction
+    super.init(nibName: nil, bundle: nil)
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  
+  // MARK: - Lifecycle
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    setup()
+    bind()
+  }
+  
+  
+  // MARK: - Setups
+  
+  private func setup() {
+    setupView()
+    setupLayout()
+  }
+  
+  private func bind() {
+    confirmButton.tapPublisher.sink { [weak self] in
+      self?.confirmAction()
+    }
+    .store(in: &cancellables)
+    
+    cancelButton.tapPublisher.sink { [weak self] in
+      self?.dismiss(animated: true)
+      self?.cancelAction?()
+    }
+    .store(in: &cancellables)
+  }
+}
+
+
+// MARK: - UI & Layouts
+
+extension PopUpViewController {
+  
+  private func setupView() {
+    view.backgroundColor = .black.withAlphaComponent(0.6)
+  }
+  
+  // MARK: - Setup Layout
+  
+  private func setupLayout() {
+    view.addSubview(contentView)
+    contentView.snp.makeConstraints {
+      $0.centerY.equalToSuperview()
+      $0.horizontalEdges.equalToSuperview().inset(24)
+    }
+    
+    contentView.addSubview(titleDescriptionStackView)
+    titleDescriptionStackView.snp.makeConstraints {
+      $0.horizontalEdges.equalToSuperview().inset(16)
+      $0.top.equalToSuperview().inset(48)
+    }
+    
+    contentView.addSubview(buttonStackView)
+    buttonStackView.snp.makeConstraints {
+      $0.top.equalTo(titleDescriptionStackView.snp.bottom).offset(36)
+      $0.horizontalEdges.bottom.equalToSuperview().inset(16)
+      $0.height.equalTo(44)
+    }
+  }
+}
+
+// MARK: - Preview
+#if canImport(SwiftUI) && DEBUG
+import SwiftUI
+
+struct PopUpViewController_Preview: PreviewProvider {
+  static var previews: some View {
+    PopUpViewController(titleText: "네이버 지도에서 여시겠어요?",
+                        descriptionText: "확인을 누르신다면 이동을 할 것이에요.\n확인을 누르신다면 이동을 할 것이에요.",
+                        confirmAction: {}).toPreview()
+  }
+}
+#endif
