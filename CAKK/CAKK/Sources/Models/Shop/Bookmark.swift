cdf5dd9b7762820ecd4ef05ef02f1be7ecaf5b91
Mason Kim <qwerty3345@naver.com>
2023-08-07 13:06:08 +0900

    [Feat] 북마크 -> Entity 변환 로직 구현

--- CAKK/CAKK/Sources/Models/Shop/Bookmark.swift
+++ CAKK/CAKK/Sources/Models/Shop/Bookmark.swift
@@ -7,10 +7,28 @@
 
 import Foundation
 
-struct Bookmark: Decodable {
+struct Bookmark: Decodable, Hashable {
   let id: Int
   let name: String
   let district: District
   let location: String
   let imageUrls: [String]
+}
+
+// MARK: - Model Mapping
+
+import RealmSwift
+
+extension Bookmark {
+  func toEntity() -> BookmarkEntity {
+    let imageUrls = imageUrls.reduce(into: List<String>(), { result, imageUrl in
+      result.append(imageUrl)
+    })
+    
+    return BookmarkEntity(id: id,
+                          name: name,
+                          district: district.rawValue,
+                          location: location,
+                          imageUrls: imageUrls)
+  }
 }
